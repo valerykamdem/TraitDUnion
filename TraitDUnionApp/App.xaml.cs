bb2627a96c36b3b93e70eae3516ec9d5e7c5f960
Valery Kamdem <valery.kamdem@celes-systems.com>
2024-04-02 00:13:18 +0200

    Add project files.

---
+++ TraitDUnionApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TraitDUnionApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TraitDUnionApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TraitDUnionApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
